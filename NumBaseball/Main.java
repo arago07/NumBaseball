9577aa49dc4a1b8a614f88d72cab5f234cf06859
arago07 <arago0712@gmail.com>
2024-09-19 20:07:11 +0900

    Numbaseball Lv3

--- NumBaseball/Main.java
+++ NumBaseball/Main.java
@@ -1,28 +1,40 @@
 package NumBaseball;
 
 
+import java.util.ArrayList;
+
 public class Main {
     public static String conclusion = "no";
     public static boolean repeat = true;
 
+    public static Integer gameNums = 0;
+    public static ArrayList<Integer> tryNum = new ArrayList<>();
     public static void main(String[] args) {
 
         Preparing preparing = new Preparing();
         Printing printing = new Printing(preparing);
 
         while (repeat) {
-            preparing.getOriginalList();
 
-            while (conclusion.equals("no")) {
-                printing.guide();
-                if (preparing.inputList == null) {
-                    break;
-                } else {
-                    printing.checkSbo();
-                    printing.if3();
-                }
+            tryNum.add(0);
 
+            ArrayList originalList = preparing.getOriginalList();
+
+            if (!originalList.contains(0)) {
+                while (conclusion.equals("no")) {
+                    printing.guide();
+
+                    if (printing.inputList == null || printing.inputList.isEmpty()) {
+                        break;
+
+                    } else {
+                        printing.checkSbo();
+                        printing.if3();
+                    }
+            }continue;
             }
+            conclusion = "no";
+            gameNums += 1;
         }
     }
 }
--- NumBaseball/Printing.java
+++ NumBaseball/Printing.java
@@ -7,64 +7,97 @@ import java.util.Scanner;
 
 public class Printing {
     private Preparing preparing;
+    boolean rInput;
 
     public Printing(Preparing preparing) {
         this.preparing = preparing;
     }
+
     ArrayList<Integer> inputList = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
 
     public ArrayList<Integer> guide() {
-
-        boolean rInput = false;
-        Scanner sc = new Scanner(System.in);
-        try{
+        rInput = false;
+        try {
             System.out.println("환영합니다. 원하시는 번호를 입력해주세요. 1. 게임 시작하기 2. 게임 기록 보기 3. 종료하기");
             int choose123 = sc.nextInt();
-                while (!rInput) {
-                    try {
-                        if (choose123 == 1) {
-                            System.out.println("3자리 숫자를 입력해주세요. \n이때 각 숫자는 1~9의 자연수이며, 동일한 숫자는 사용할 수 없습니다.");
-                            int input = sc.nextInt();
-
-                            if (input >= 100 && input < 999) {
-                                inputList.add(input / 100);
-                                inputList.add((input / 10) % 10);
-                                inputList.add(input % 10);
-
-                                preparing.setInputList(inputList);
-                                if (inputList.get(0).equals(inputList.get(1)) || inputList.get(1).equals(inputList.get(2)) || inputList.get(2).equals(inputList.get(0))) {
-                                    System.out.println("올바르지 않은 입력입니다.");
-                                    inputList.clear();
-                                } else {
-                                    rInput = true;
-                                }
-                            } else if (input < 100 || input > 999) {
-                                System.out.println("올바르지 않은 입력입니다.");
-                            }
-                        } else if (choose123 == 2) {
-                            System.out.println("해당 기능은 Lv3에서 제시됩니다.");
-                            inputList.clear();
-                            return inputList;
-                        } else if (choose123 == 3) {
-                            System.out.println("게임을 종료합니다.");
-                            System.exit(0);
+            sc.nextLine();
+            while (!rInput) {
+                try {
+                    if (choose123 == 1) {
+                        num3();
+                    } else if (choose123 == 2) {
+                        System.out.println("<게임 기록 보기>");
+                        if (Main.gameNums == 1) {
+                            System.out.println("시도한 기록이 없습니다.");
+                            rInput = true;
                         } else {
-                            System.out.println("올바르지 않은 입력입니다. 다시 입력해주세요.");
-                            sc.next();
+                            for (int i = 0; i-1 < Main.gameNums; i++) {
+                                System.out.println((i+1) + "번째 게임 : 시도 횟수 - " + Main.tryNum.get(i));
+                            }
+                            rInput = true;
                         }
-                    } catch (InputMismatchException e) {
+                    } else if (choose123 == 3) {
+                        System.out.println("게임을 종료합니다.");
+                        System.exit(0);
+                    } else {
                         System.out.println("올바르지 않은 입력입니다. 다시 입력해주세요.");
-                        sc.next();
+                        inputList.clear();
+                        rInput = true;
+
                     }
-
+                } catch (InputMismatchException e) {
+                    System.out.println("올바르지 않은 입력입니다. 다시 입력해주세요.");
+                    inputList.clear();
+                    Main.conclusion = "yes";
                 }
-            } catch (InputMismatchException e) {
+
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("올바르지 않은 입력입니다.");
+            inputList.clear();
+            Main.conclusion = "yes";
+        }
+        return inputList;
+    }
+
+    public boolean num3() {
+        Main.tryNum.set(Main.gameNums, Main.tryNum.get(Main.gameNums+ 1));
+
+        try {
+            System.out.println("3자리 숫자를 입력해주세요. \n이때 각 숫자는 1~9의 자연수이며, 동일한 숫자는 사용할 수 없습니다.");
+            int input = sc.nextInt();
+            sc.nextLine();
+
+            if (input >= 100 && input < 999) {
+                inputList.add(input / 100);
+                inputList.add((input / 10) % 10);
+                inputList.add(input % 10);
+
+                preparing.setInputList(inputList);
+                if (inputList.get(0).equals(inputList.get(1)) || inputList.get(1).equals(inputList.get(2))
+                        || inputList.get(2).equals(inputList.get(0)) || inputList.contains(0)) {
+                    System.out.println("올바르지 않은 입력입니다.");
+                    inputList.clear();
+                } else {
+                    rInput = true;
+                }
+            } else {
                 System.out.println("올바르지 않은 입력입니다.");
-                sc.next();
+                inputList.clear();
 
-            }return inputList;
+            }
+            return false;
 
-}
+        } catch (InputMismatchException e) {
+            System.out.println("올바르지 않은 입력입니다.");
+            sc.nextLine();
+            inputList.clear();
+        }
+        return false;
+    }
+
+
 
 
 
@@ -91,11 +124,14 @@ public class Printing {
         ArrayList<Integer> sboList = preparing.getSboList();
         ArrayList<Integer> inputList = preparing.getInputList();
         if (sboList.get(0).equals(3)) {
+            inputList.clear();
+            preparing.resetSboList();
             System.out.println("정답을 맞추셨습니다! 축하드립니다.");
             return Main.conclusion = "yes";
         } else {
             inputList.clear();
             preparing.resetSboList();
+            Main.tryNum.set(Main.gameNums, (Main.tryNum.get(Main.gameNums)) + 1);
             return Main.conclusion = "no";
         }
     }
